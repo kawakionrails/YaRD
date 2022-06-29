db61b35c7142de4e0790705de31949403d4a1dc4
kawakionrails <erick_nara@icloud.com>
2022-06-29 14:17:41 -0300

    add: view binding

--- app/src/main/java/io/github/kawaki/yard/MainActivity.kt
+++ app/src/main/java/io/github/kawaki/yard/MainActivity.kt
@@ -2,12 +2,16 @@ package io.github.kawaki.yard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.kawaki.yard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 }
